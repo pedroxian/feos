1027f12e3182883526e4622640612c50338fdc94
pedroxian <44950875+pedroxian@users.noreply.github.com>
2020-08-28 12:03:46 +0200

    commit

---
+++ spod-else.js
@@ -0,0 +1,28 @@
+    1  clear
+    2  set
+    3  set in.action-.auto -x /C2.k estate2.ovm -sat.m:controll -n-x
+    4  set
+    5  set training.-dudesh _dogy.ashami?extract{execute.codes -ux -c
+    6  command
+    7  set trade
+    8  set
+    9  set acomplish.complex -ashima.dudesh -riot?ajax prepare.products
+   10  set minimal.products{compress.granularizer >script.go -else.ova
+   11  command
+   12  set
+   13  set use.standard>exact.2 -execute/route _build.ova-use {config
+   14  set
+   15  set shelter-virtual.ova=runtime_compare{standard.exact -all.brands
+   16  set mac.course>student i.mac -loose /inform.cue -run.rom{execute
+   17  command
+   18  set
+   19  set .c
+   20  command
+   21  set
+   22  set go{doppler -sx
+   23  command
+   24  set
+   25  set culp{ova _runtime.start -exceed --vans /trade.use -vpx -es1
+   26  set es1.mk2 _troll.nuo -exceed.odd /trd -.open /send.gov
+   27  set user.form?troll -else.if{true.execute -nano _let.sold -sx
+   28  command
